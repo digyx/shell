9d7b10d5f57288ecd2847454bac6a0c8f15b949b
Devon Tingley <dtingley@twilit.io>
2021-08-16 09:40:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,34 @@ sudo chown -R $(whoami) ~/.oh-my-zsh ~/.zshrc
 sudo python3 -m pip install thefuck
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Copy .zshrc file, setup z
+# Copy dotfiles
 cp .zshrc ~/.zshrc
 touch ~/.z
+
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
+
+setup_macos() {
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+}
+
+# Determine which OS is in use
+case uname in
+    Linux*)   INSTALL_CMD="yay -S";;
+    Darwin*)  INSTALL_CMD="brew install";;
+esac
+
+# Install software
+$INSTALL_CMD neovim
+$INSTALL_CMD rustup rust-analyzer
+$INSTALL_CMD go gopls
+$INSTALL_CMD nodejs yarn
+$INSTALL_CMD cloc
+
+if [[ uname == "Linux" ]]; then
+    $INSTALL_CMD wine-staging evince neofetch
+    $INSTALL_CMD docker docker-compose
+    $INSTALL_CMD texlive-full
+elif [[ uname == "Darwin" ]]; then
+    $INSTALL_CMD htop
