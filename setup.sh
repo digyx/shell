3a5cb0cc4b81c6b5e7b62bb11b74b52ec853c194
Devon Tingley <dtingley@twilit.io>
2022-01-24 21:32:52 -0500

    Switch to fish, fix script for dotfiles, update install list

--- setup.sh
+++ setup.sh
@@ -1,20 +1,12 @@
-# Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo chown -R $(whoami) $HOME/.oh-my-zsh $HOME/.zshrc
-
-# Install thefuck, zsh-suggestion
-sudo python3 -m pip install thefuck
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Copy dotfiles
-ln -sn $PWD/.config $HOME/.config
-ln -s $PWD/.zshrc $HOME/.zshrc
-touch $HOME/.z
-
+#!/bin/bash
 setup_macos() {
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 }
+
+for conf in $(ls dotfiles); do
+    ln -sn $PWD/dotfiles/$conf $HOME/.config/$conf
+done
 
 # Determine which OS is in use
 case "$(uname)" in
@@ -23,16 +15,15 @@ case "$(uname)" in
 esac
 
 # Install software
-$INSTALL_CMD git cloc htop
-$INSTALL_CMD neovim
+$INSTALL_CMD fish git cloc htop ncdu tree
+$INSTALL_CMD alacritty neovim zellij zoxide
 $INSTALL_CMD rustup rust-analyzer
 $INSTALL_CMD go gopls
 $INSTALL_CMD nodejs yarn
+$INSTALL_CMD obsidian
 
 if [[ "$(uname)" == "Linux" ]]; then
-    $INSTALL_CMD lxappearance
-    $INSTALL_CMD adapta-gtk-theme papirus-icon-theme nerd-fonts-complete
-    $INSTALL_CMD evince neofetch feh
+    $INSTALL_CMD rofi
+    $INSTALL_CMD lxappearance adapta-gtk-theme papirus-icon-theme nerd-fonts-complete
     $INSTALL_CMD docker docker-compose
-    $INSTALL_CMD texlive-full
 fi
