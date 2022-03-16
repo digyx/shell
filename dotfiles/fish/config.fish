12c5a4d2b5ca753e46721741783c6b15fd12a9d5
Devon Tingley <dtingley@twilit.io>
2022-03-16 18:16:39 -0400

    Laptop configs

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,6 +1,11 @@
 set -x CHROME_EXECUTABLE /usr/bin/chromium
 set -x EDITOR /usr/bin/nvim
 
+if [ $hostname = "Arcadia-III" ]
+    set -x GDK_SCALE 2
+    set -x GDK_DPI_SCALE 0.5
+end
+
 if status is-interactive
     zoxide init fish | source
 end
